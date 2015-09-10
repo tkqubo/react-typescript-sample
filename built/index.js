4704b453ca130e16d59971ccee8382686ae1c3aa
tkqubo <tk.qubo@gmail.com>
2015-09-11 03:32:21 +0900

    Create sample tsx file anyway

---
+++ built/index.js
@@ -0,0 +1,7 @@
+var React = require('react');
+var a = 3;
+var b = React.createElement("div", null);
+var c = React.createElement("div", {"className": "hoge"});
+console.dir(a);
+console.dir(b);
+console.dir(c);
---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+let a = 3;
+let b: any = <div></div>;
+let c = <div className="hoge"></div>
+
+console.dir(a);
+console.dir(b);
+console.dir(c);
